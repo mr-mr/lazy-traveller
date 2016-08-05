d4ac7b309f9d33b7a721c04c14ab1736bd11324c
Chris J Clarke <mrmr.mail@gmail.com>
2016-08-05 12:28:39 +0100

    running example of app

---
+++ gruntfile.js
@@ -0,0 +1,73 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    sass: {
+      dist: {
+        files: {
+          'css/global.css' : 'src/_styles/global.scss'
+        }
+      }
+    },
+    // copy: {
+    //   build: {
+    //     cwd: 'src',
+    //     src: [ '**' ],
+    //     dest: 'build',
+    //     expand: true
+    //   },
+    // },
+    connect: {
+      server: {
+        options: {
+          port: 8000,
+          hostname: '*',
+          onCreateServer: function(server, connect, options) {
+            var io = require('socket.io').listen(server);
+            io.sockets.on('connection', function(socket) {
+              // do something with socket
+            });
+          }
+        }
+      }
+    },
+    watch: {
+      css: {
+        files: '**/*.scss',
+        tasks: ['sass']
+      },
+      scripts: {
+          files: '**/*.js',
+          tasks: ['uglify']
+      }
+    },
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'style',
+          src: ['global.css', '!global.min.css'],
+          dest: 'build',
+          ext: '.min.css'
+        }]
+      }
+    },
+    uglify: {
+            dist: {
+                files:{
+                    'js/_api/tflapi.min.js': 'src/_api/tflapi.js',
+                }
+            }
+        },
+  });
+
+  grunt.loadNpmTasks('grunt-notify');
+  grunt.loadNpmTasks('grunt-execute');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+
+  grunt.registerTask('default',['sass', 'connect', 'watch', 'cssmin', 'uglify']);
+}
